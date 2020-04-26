c3af550af855755c16d2a715e62d6d5575b2293b
Yangzi-Wang <1077692618@qq.com>
2020-04-27 01:12:18 +0800

    修改anim类

---
+++ miniprogram/js/base/myanim.js
@@ -0,0 +1,84 @@
+export default class MyAnimation {
+  constructor(imgObj, count, resource_w, resource_h) {
+    
+    // 当前动画是否播放中
+    this.isPlaying = false
+
+    // 动画是否需要循环播放
+    this.loop = false
+
+    // 每一帧的时间间隔
+    this.interval = 20
+
+    // 当前播放的帧
+    this.index = -1
+
+    // 总帧数
+    this.count = count
+
+    this.img = imgObj
+    this.img_w = resource_w / this.count
+    this.img_h = resource_h
+  }
+
+  init(locx, locy, target_w, target_h) {
+    this.locx = locx
+    this.locy = locy
+    this.target_w = target_w
+    this.target_h = target_h
+
+    this.isPlaying = false
+    this.index = 0
+  }
+
+  // 将播放中的帧绘制到canvas上
+  render(ctx) {
+    
+    ctx.drawImage(
+      this.img,
+      // 源图像
+      this.index*this.img_w,
+      0,
+      this.img_w,
+      this.img_h,
+      // 目标图像
+      this.locx,
+      this.locy,
+      this.target_w,
+      this.target_h
+    )
+  }
+
+  // 播放预定的帧动画
+  playAnimation(index = 0, loop = false, interval = 20) {
+
+    this.isPlaying = true
+    this.loop = loop
+    this.interval = interval
+    this.index = index
+  }
+
+  // 停止帧动画播放
+  stop() {
+    this.isPlaying = false
+  }
+
+  // 帧遍历
+  update() {
+    if(this.isPlaying){
+      this.index++
+
+      if (this.index > this.count - 1) {
+        if (this.loop) {
+          this.index = 0
+        }
+
+        else {
+          this.index--
+          this.stop()
+        }
+      }
+    }
+    
+  }
+}
--- miniprogram/js/scene/selectrole.js
+++ miniprogram/js/scene/selectrole.js
@@ -1,5 +1,10 @@
+import MyAnimation from '../base/myanim.js'
 let bedroomBg = new Image()
 bedroomBg.src = 'images/bedroom.jpg'
+let boy = new Image()
+boy.src = 'images/role-boy.png'
+let girl = new Image()
+girl.src = 'images/role-girl.png'
 export default class SelectRole {
   constructor(databus) {
 
@@ -11,24 +16,39 @@ export default class SelectRole {
       width: 100,
       height: 50
     }
+
+    this.animations = []
+    this.boy = new MyAnimation(boy, 7, 2237, 484)
+    this.animations.push(this.boy)
+    this.girl = new MyAnimation(girl, 7, 2239, 484)
+    this.animations.push(this.girl)
+
   }
   init() {
-    this.animations = []
+    //this.animations = []
+    this.boy.init(canvas.width/2-100, 90, 319*0.3,484*0.3)
+    this.boy.playAnimation(0,true,20)
+    this.girl.init(canvas.width /2+10, 90, 319 * 0.3, 484 * 0.3)
+    this.girl.playAnimation(0, true, 20)
     this.bindEvent()
+    
   }
   render(ctx) {
     ctx.drawImage(bedroomBg,0,0,canvas.width,canvas.height)
     ctx.fillStyle = '#ff0000'
     ctx.fillRect(this.btnArea.startX, this.btnArea.startY, this.btnArea.width, this.btnArea.height)
 
-    // this.animations.forEach((ani) => {
-    //     if (ani.isPlaying) {
-    //       ani.aniRender(ctx)
-    //     }
-    //   })
+    this.animations.forEach((ani) => {
+        
+          ani.render(ctx)
+        
+      })
   }
   update(){
-    
+    this.animations.forEach((ani) => {
+      if(this.databus.frame % ani.interval ===0)
+      ani.update()
+    })
   }
 
   bindEvent() {
